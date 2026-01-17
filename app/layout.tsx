5d6c2e6b191fced71bb5546c4637cee8db8b9e28
Adeel Ali Yousaf <adeelaliyousaf01@gmail.com>
2026-01-18 01:54:00 +0500

    Updated Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,12 +64,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Preconnects for Google Fonts to speed up font loading */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        {/* Preload the stylesheet for Poppins (non-blocking) */}
-            <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" />
-            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <noscript>
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        </noscript>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
         {/* JSON-LD Organization schema for SEO (logo in /public/SmartScanPlusLogo.png) */}
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
           "@context": "https://schema.org",
